02b849ac7debc0616ed3f1f04691b160e30d5e57
ZoranMitkovski <zoran.mitkovski@ledgerleopard.com>
2025-10-19 18:53:57 +0200

    Enable flyway and add postgres keycloak image

--- src/main/java/org/ivy/IveEventsApplication.java
+++ src/main/java/org/ivy/IveEventsApplication.java
@@ -1,11 +1,11 @@
 package org.ivy;
 
-//TIP To Run code, press the Run action or click the run icon in the gutter.
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class IveEventsApplication {
     public static void main(String[] args) {
-        //TIP Press Show Intention Actions with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
-
+        SpringApplication.run(IveEventsApplication.class, args);
     }
 }
