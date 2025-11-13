c891f6dc520955b8dcb71d83202bf195ec4bfa72
ZoranMitkovski <zoran.mitkovski@ledgerleopard.com>
2025-11-13 18:21:24 +0100

    Remove Band

--- src/main/java/org/ivyinc/eventplanner/event/repository/BandRepository.java
+++
@@ -1,9 +0,0 @@
-package org.ivyinc.eventplanner.event.repository;
-
-import org.ivyinc.eventplanner.common.BaseRepository;
-import org.ivyinc.eventplanner.event.model.Band;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BandRepository extends BaseRepository<Band> {
-}
